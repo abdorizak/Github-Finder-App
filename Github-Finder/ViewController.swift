bbe135125951b224e848a54605e97be5bb9b70df
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-10-21 07:30:15 +0300

    Initial Commit

---
+++ Github-Finder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Github-Finder
+//
+//  Created by Abdirizak Hassan on 10/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
